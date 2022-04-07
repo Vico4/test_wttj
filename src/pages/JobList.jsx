a9751ed2113e2f463c9915da686301acdcab8f8c
Vico4 <victoire.stahl@gmail.com>
2022-04-07 23:12:00 +0200

    fetching jobList

--- src/pages/JobList.jsx
+++ src/pages/JobList.jsx
@@ -8,18 +8,43 @@ import { Text } from '@welcome-ui/text'
 import { useState, useEffect } from 'react'
 
 
-
 function JobList() {
+
+    const [jobList, getJobList] = useState({})
+    const [isLoading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+
+    useEffect(() => {
+        async function fetchData() {
+          try {
+            const response = await fetch('https://www.welcomekit.co/api/v1/embed?organization_reference=Pg4eV6k')
+            const data = await response.json()
+            console.log(data.jobs)
+            getJobList(data.jobs)
+          } catch (err) {
+            console.log(err)
+            setError(true)
+          } finally {
+            setLoading(false)
+          }
+        }
+        setLoading(true)
+        fetchData()
+      }, [])
+
 
     return(
         <Box display="flex" w="100%" justifyContent="center" alignItems="center" backgroundColor="nude.100">
-        <Stack>
-        <Text variant='h1'>Welcome to the jungle</Text>
-        <Searchbar />
-        <JobInfos />
-        </Stack>
-        </Box>
-        
+            <Stack>
+                <Text variant='h1'>Welcome to the jungle</Text>
+                <Searchbar />
+                {isLoading ? 
+                    (<Loader/>) : 
+                    (<>{jobList.map((job) => (<p>{job.name}</p>))}</>)
+                    }
+                <JobInfos />
+            </Stack>
+        </Box> 
     )
 }
 
