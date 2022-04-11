2d9532b76bab6870f20ac701e00c6e943217de5b
Vico4 <victoire.stahl@gmail.com>
2022-04-11 17:07:30 +0200

    added salary and start date to job details when exist

--- src/components/Jobdetails.jsx
+++ src/components/Jobdetails.jsx
@@ -9,11 +9,17 @@ function JobDetails(props) {
     return(
         <>
         <JobInfos {...props}/>
+        <Text variant="body1">{props.start_date ? 
+            (props.office.name === "Paris" ? 
+                "date de début: " + props.start_date.fr :
+                "starting on the " + props.start_date.en) : 
+                null}</Text>
         <Text variant="h4"> Description </Text>
         <div dangerouslySetInnerHTML={{__html: props.description}}></div>
         <Text variant="h4"> Profil recherché </Text>
         <div dangerouslySetInnerHTML={{__html: props.profile}}></div>
-        <div>{props.salary.min && props.salary.min}</div>
+        <div>{props.salary.min && "salaire minimum : " + props.salary.min + props.salary.currency}</div>
+        <div>{props.salary.min && "salaire maximum : " + props.salary.max}</div>
         <Box w="100%" display="flex" justifyContent="center" alignItems="center">
         <Button as="a" href={props.url} target="_blank" > Apply </Button>
         </Box>
